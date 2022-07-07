111ce7b0c92c333ab67740b99ddddb82ca50b08a
crystalmori <crystalmori@outlook.com>
2022-07-07 16:24:21 -0700

    fix bugs

--- feedback-app/src/context/FeedbackContext.jsx
+++ feedback-app/src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
