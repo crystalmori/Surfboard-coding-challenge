a061ac8176417baa1f8866f612d4cb5e1426f550
crystalmori <crystalmori@outlook.com>
2022-07-06 21:12:36 -0700

    add Routes to About Page

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,6 +5,8 @@ import FeedbackData from "./components/data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from 'uuid'
+import AboutPage from "./pages/AboutPage";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
 
@@ -22,16 +24,23 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header/>
             <div className='container'>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback}
-                    handleDelete={deleteFeedback}
+            <Routes>
+                <Route exact path='/' element= {
+                    <>
+                         <FeedbackForm handleAdd={addFeedback}/>
+                        <FeedbackStats feedback={feedback}/>
+                        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}
                 />
-            </div>
-        </>
+                    </>
+                }>
+               </Route>
+                <Route path='/about' element={<AboutPage/>} />
+            </Routes>
+            </div>   
+        </Router>
     )
 }
 
